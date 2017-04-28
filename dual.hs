7c7a1134d4c2fe0624ebc96df3889691767782c5
Andrew Lei <andrew.w.lei@gmail.com>
2017-04-28 07:08:18 -0700

    added comments + slight speed boost from a modification to roots

--- dual.hs
+++ dual.hs
@@ -43,22 +43,32 @@ instance Floating Dual where
   exp (Dual a b) = Dual (exp a) $ b * exp a
   log (Dual a b) = Dual (log a) $ b/a
 
+-- makevar: Converts a Double to a variable with value of the Double
 makevar :: Double -> Dual
 makevar x = Dual x 1.0
 
+-- deriv: Get derivative of function
 deriv :: (Dual -> Dual) -> Double -> Double
 deriv f = inf . f . makevar
 
+-- legendre: Legendre polynomial of order n at x
+-- Legendre order 0 is 1
 legendre :: (Fractional a) => Int -> a -> a
 legendre 0 _ = 1
 legendre n x = helper 1 x n
   where
+    -- Legendre is defined recursively in terms of
+    -- lower order Legendre polynomials
     helper _    acc2 1 = acc2
     helper acc1 acc2 n = helper acc2 rec (n-1)
       where
         n' = fromIntegral n
+        -- recursive definition of Legendre polynomial:
+        -- n P_{n}(x) = (2n - 1) x P_{n-1}(x) - (n-1) P_{n-2}(x)
         rec = ((2*n' - 1) * x * acc2 - (n'-1) * acc1)/n'
 
+-- hermite: Hermite polynomial of order n at x
+-- hermite order 0 is 1
 hermite :: (Fractional a) => Int -> a -> a
 hermite 0 _ = 1
 hermite n x = helper 1 (2*x) n
@@ -67,33 +77,59 @@ hermite n x = helper 1 (2*x) n
     helper acc1 acc2 n = helper acc2 rec (n-1)
       where
         n' = fromIntegral n
+        -- recursive definition of Hermite polynomial (physicists'):
+        -- H_n(x) = 2 x H_{n-1}(x) - H'_{n-1}(x)
+        -- H'_n(x) = 2 n H_{n-1}(x)
+        -- H_n(x) =  2 x H_{n-1}(x) - 2 (n-1) H_{n-2}(x)
         rec = 2 * x * acc2 - 2 * (n' - 1) * acc1
 
+-- roots: Multiple root finder using (a variant of) Newton's method
+-- (forall a. Fractional a => (a -> a)) guarantees that function whose roots
+-- we're looking for is guaranteed to have a derivative, since (Dual -> Dual)
+-- is an instance (or something) of Fractional a => (a -> a)
+-- This is based on the algorithm in this paper:
+-- http://www.kurims.kyoto-u.ac.jp/EMIS/journals/AMI/2006/barrera.pdf
 roots :: Double -> (forall a. Fractional a => (a -> a)) -> Int -> Double -> [Double]
-roots tol f nroots lowbd = helper lowbd []
+roots tol f nroots lowbd = helper nroots lowbd []
   where
-    helper :: Double -> [Double] -> [Double]
-    helper lowbd zeros
-      | length zeros == nroots = zeros
-      | otherwise = helper (newzero + tol) (newzero:zeros)
+    -- helper: If number of zeros equals nroots, return the zeros
+    -- Otherwise, find another zero, add it to the list, and begin
+    -- searching for the next value, a little bit above the previous value
+    helper :: Int -> Double -> [Double] -> [Double]
+    helper 0 _     zeros = zeros
+    helper n lowbd zeros = helper (n-1) (malu newzero) (newzero:zeros)
       where
         malu :: Double -> Double
         -- Move A Little Up
         -- Otherwise you're stuck and will get divide by zero
         malu x
-          | x < 0 = x * (1 - 2**(-53))
-          | x == 0 = 2**(-1074)
-          | x > 0 = x * (1 + 2**(-52))
+          | x < 0 = x * (1 - tol)
+          | x == 0 = tol
+          | x > 0 = x * (1 + tol)
 
         newzero :: Double
         newzero = helper' lowbd
 
+        -- helper': Implementation of Newton's Method (or, well, a variant)
         helper' :: Double -> Double
         helper' guess
-          | (abs . real . f . makevar) guess < tol = guess
-          --x_i = x_i - f(x_i) / (f'(x_i) - f(x_i) * sum_{j < i}(1 / (x_i - x_j)))
+          -- If |f(x)| < tol, it's good enough
+          | (abs . f) guess < tol = guess
+          -- Otherwise, iterate again
           | otherwise = helper' guess'
           where
+            -- Traditionally, Newton's Method is x = x - f(x)/f'(x)
+            -- What if you want to find more than one root?
+            -- Factor out the previous roots you've found!
+            -- g(x) = f(x) / Π_{j<i} (x - x_j)
+            -- prod = Π_{j<i} (x - x_j)
+            -- d/dx prod = Π_{j<i, j≠0} (x - x_j) + Π_{j<i, j≠1} (x - x_j) + ...
+            --           = prod Σ_{j < i}(1 / (x - x_j))
+            -- g'(x) = (f'(x)prod - f(x) d/dx prod) / prod^2
+            --       = (f'(x) - f(x)Σ_{j < i}(1 / (x - x_j))) / prod
+            -- So just find the roots of g!
+            -- x_i = x_i - g(x_i) / g'(x_i)
+            --     = x_i - f(x_i) / (f'(x_i) - f(x_i) Σ_{j < i}(1 / (x_i - x_j)))
             guess' :: Double
             guess' = guess - fguess / (f'guess - fguess * transformedSum zeros)
 
@@ -104,6 +140,7 @@ roots tol f nroots lowbd = helper lowbd []
             transformedSum :: [Double] -> Double
             transformedSum = foldl transformation 0
 
+-- gaussQuadrature: Σ w/ω f(x)
 gaussQuadrature :: [(Double, Double, Double)] -> Int -> (Double -> Double) -> Double
 gaussQuadrature polyVals n f = sum $ map (apply f) polyVals
   where
@@ -158,8 +195,8 @@ gaussian x = 1 / (sqrt (2*pi)) * exp (-x*x/2)
 main = do
     --print $ rs
     --print $ minimum diffs
-    --print $ maximum zs
-    print $ ((ghtn (\x -> exp (-x*x)))^2 - pi)/pi
+    print $ maximum zs
+    --print $ ((ghtn (\x -> exp (-x*x)))^2 - pi)/pi
     --print $ gltn gaussian (-1,1)
     --print $ gltn gaussian (-1.96,1.96)
     --print $ gltn gaussian (-2,2)
@@ -172,11 +209,11 @@ main = do
         a :: Dual
         a = y*fromInteger x
 
-        n = 15
+        n = 383
         ln = legendre n
         hn = hermite n
 
-        tol = (10**(-2))
+        tol = (10**(-12))
 
         rootFinder = roots tol
         lFinder = rootFinder ln n
@@ -184,11 +221,11 @@ main = do
         hermiteLB n = - (n-1) * sqrt (2 / (n+2))
 
         rs :: [Double]
-        --rs = lFinder (-1.0)
-        rs = hFinder (hermiteLB n)
+        rs = lFinder (-1.0)
+        --rs = hFinder (hermiteLB n)
 
         zs :: [Double]
-        zs = map hn rs
+        zs = map ln rs
 
         rs' = sort rs
         diffs = zipWith (-) (tail rs') rs'
